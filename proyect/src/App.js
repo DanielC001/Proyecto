9af42b71cc7c736019755e3ee775428ec087c4a3
DanielC001 <danielcaro1000mail.com>
2021-11-02 20:09:50 -0500

    a4

--- proyect/src/App.js
+++ proyect/src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+<public />
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <ul class="navbar">
+          <li>
+            <img src="./img/gwentit.png" alt="logo" class="logo" />
+          </li>
+          <li>
+            <button class="button mainB">Nosotros</button>
+          </li>
+          <li>
+            <div class="buscar">
+              <input type="text" placeholder="Buscar" />
+              <i class="fas fa-search button iconlupa"></i>
+            </div>
+          </li>
+          <li>
+            <button class="button mainB">Iniciar sesión</button>
+          </li>
+          <li>
+            <button class="button secondB">Registro</button>
+          </li>
+        </ul>
       </header>
+      <main>
+        <section>
+          <h1>Titulo</h1>
+          <ul class="lista">
+            <Tarjetas nombre="E1" imagen={logo}></Tarjetas>
+            <Tarjetas nombre="E2" imagen={logo}></Tarjetas>
+            <Tarjetas nombre="E3" ></Tarjetas>
+          </ul>
+        </section>
+      </main>
+      <footer></footer>
     </div>
   );
 }
 
+/*
+let -> variable que va a cambiar mas adelante
+const -> 
+objeto en javascript -> parecido a el diccionario de pyton{clave:"valor",clave:"valor"}
+
+*/ 
+
+function Tarjetas(props) {
+  return (
+    <li class="sep">
+      <div class="cont-imagen">
+        <img src={props.imagen} alt="img" />
+      </div>
+      <span class="text">{props.nombre}</span>
+    </li>
+  );
+}
 export default App;
